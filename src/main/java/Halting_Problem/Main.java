ab224c9e3339263a7a7658f4e5ab630fa245634c
marcos <marcgabe@myuax.com>
2024-04-30 19:13:44 +0200

    Montaje y preparación para Javafx

---
+++ src/main/java/Halting_Problem/Main.java
@@ -0,0 +1,8 @@
+package Halting_Problem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.marcos_gb4pruebadeevaluacioncontinua2942024 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens Halting_Problem to javafx.fxml;
+    exports Halting_Problem;
+}
